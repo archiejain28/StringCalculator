6768b24c265bc3ca19440b1a68f23117796a7c04
Archie Jain <jain.28.archie@gmail.com>
2024-06-23 13:09:43 +0530

    case 2: add method to handle any amount of numbers(TDD)

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,10 @@ import {StringCalculator} from './index';
 describe('Add method of String Calculator',()=>{
     test('Should return zero when empty string given',()=>{
         const calc = new StringCalculator()
-        expect(calc.add('')).toEqual(0)
+        expect(calc.add('')).toEqual(0);
+    })
+    test('Should return sum when string comma-separated numbers given',()=>{
+        const calc = new StringCalculator()
+        expect(calc.add('1,6,8')).toEqual(15);
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,18 @@ export class StringCalculator {
     if (numbers.length == 0 || numbers == null) {
         return 0
     }
+    if(numbers.length > 0){
+        const newArr = numbers.split(',')
+        console.log(newArr)
+        const sum = newArr.reduce((acc, curr)=>{
+            acc = acc + parseInt(curr)
+            return acc
+        },0)
+        return sum
+    }
  }
 }
 
 const calc = new StringCalculator()
 
-console.log(calc.add(''))
+console.log(calc.add("1,2,3,5,60,70"))
